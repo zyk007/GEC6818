5001953d8eaa75cebbf343d02075a1bbc44d57db
Zhou Yongkang <40411528+zyk007@users.noreply.github.com>
2023-05-20 13:40:34 +0800

    Update touch.c

--- src/touch.c
+++ src/touch.c
@@ -16,10 +16,13 @@ int project_touch(int *th_x, int *th_y)
 		read(fd_touch, &buf, sizeof(buf));
 
 		if (buf.type == EV_ABS && buf.code == ABS_X)
+			//*th_x = buf.value;
 			*th_x = buf.value*800/1024;
 		if (buf.type == EV_ABS && buf.code == ABS_Y)
+			//*th_x = buf.value;
 			*th_y = buf.value*480/600;
-		
+		//这是开发板驱动问题，开发板的屏幕像素是800*480，但驱动给的分辨率有误，驱动的分辨率是1024*600，所以需要乘相应的值
+		//触控驱动正常的开发板无需乘这些值，即使用注释的代码，无需使用源代码
 		if (buf.type == EV_KEY && buf.code == BTN_TOUCH && buf.value == 1)
 		{
 			old_x = *th_x;
